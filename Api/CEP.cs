544f71b74632614bea9d6d726a250d93e9e7128c
ThiagoApolonio <thiagoapolonioo@hotmail.com>
2021-09-29 21:38:36 -0300

    Consumindo Api de busca de cep "https://viacep.com.br

--- Api/CEP.cs
+++ Api/CEP.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Api
 {
@@ -17,7 +19,71 @@ namespace Api
             InitializeComponent();
         }
 
-        private void CEP_Load(object sender, EventArgs e)
+        private void CEP_Leave(object sender, EventArgs e)
+        {
+                if (!String.IsNullOrEmpty(txtCEP.Text))
+            {
+                if (Regex.IsMatch(txtCEP.Text, @"^\d+$"))
+                {
+                    if (txtCEP.Text.Replace("-", "").Replace(".", "").TrimEnd().TrimStart().Length == 8)
+                    {
+                        CLcep.TexsBoxs texsBoxs = new CLcep.TexsBoxs();
+                        texsBoxs = CLcep.GetApi(txtCEP.Text.Replace("-", "").Replace("-", "").TrimEnd().TrimStart());
+                        txtLogradouro.Text = texsBoxs.logradouro;
+                        txtComplemento.Text = texsBoxs.complemento;
+                        txtBairro.Text = texsBoxs.bairro;
+                        txtLocalidade.Text = texsBoxs.localidade;
+                        txtUF.Text = texsBoxs.uf;
+                        txtUnidade.Text = texsBoxs.unidade;
+                        txtIBGE.Text = texsBoxs.ibge;
+                        txtGIA.Text = texsBoxs.gia;
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Campo de CEP tem que ter 8 digitos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Cep no formado inválido.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Cep é obrigatório.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+           
+            
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
---
+++ Api/CLcep.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Api
+{
+    class CLcep
+    {
+        private static WebClient InicializarCliente(string valorCep)
+        {
+            WebClient client = new WebClient();
+            client.BaseAddress = string.Format("https://viacep.com.br/ws/{0}/json/", valorCep);
+            client.Headers.Add("content-type", "application/json; charset=utf-8");
+            client.Encoding = Encoding.UTF8;
+            return client;
+        }
+
+        public static TexsBoxs GetApi(string valorCep)
+        {
+            WebClient client = InicializarCliente(valorCep);
+            string response = Encoding.UTF8.GetString(client.DownloadData(client.BaseAddress));
+            return (TexsBoxs)JsonConvert.DeserializeObject(response, typeof(TexsBoxs));
+        }
+
+        public class TexsBoxs
+        {
+            public string cep { get; set; }
+            public string logradouro { get; set; }
+            public string complemento { get; set; }
+            public string bairro { get; set; }
+            public string localidade { get; set; }
+            public string uf { get; set; }
+            public string unidade { get; set; }
+            public string ibge { get; set; }
+            public string gia { get; set; }
+        }
+
+    }
+}
